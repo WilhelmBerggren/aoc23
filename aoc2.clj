b56665b5ef2847c646aaaced72e1fdf250f206ed
Wilhelm Berggren <wilhelm.berggren@hiberworld.com>
2023-12-04 13:17:24 +0100

    Restructure clojure day 2

--- aoc2.clj
+++ aoc2.clj
@@ -1,13 +1,14 @@
 (ns aoc2)
 
-(defn ->rgb [line]
-  (->> line
-       (re-seq #"(\d{1,2})\s(red|green|blue)")
-       (map (fn [[_ n c]] (sorted-map (keyword c) (parse-long n))))
-       (apply merge-with max)
-       ((juxt :red :green :blue))))
-
-(let [rgbs (map ->rgb (.split (slurp "2.txt") "\n"))
-      ans1 (apply + (keep-indexed (fn [idx game] (when (every? true? (map <= game [12, 13, 14])) (inc idx))) rgbs))
-      ans2 (apply + (map (partial apply *) rgbs))]
+(let [->rgb (fn [line]
+              (->> line
+                   (re-seq #"(\d{1,2})\s(red|green|blue)")
+                   (map (fn [[_ n c]] {(keyword c) (parse-long n)}))
+                   (apply merge-with max)
+                   ((juxt :red :green :blue))
+                   ((fn [rgb] {:valid (every? true? (map <= rgb [12, 13, 14]))
+                               :product (apply * rgb)}))))
+      rgbs (map ->rgb (.split (slurp "2.txt") "\n"))
+      ans1 (apply + (keep-indexed (fn [idx game] (when (:valid game) (inc idx))) rgbs))
+      ans2 (apply + (map :product rgbs))]
   [ans1 ans2])
